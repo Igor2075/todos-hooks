d0b2bd9ee2e4313ac317117b977f5d053ace25a4
Igor2075 <39250766+Igor2075@users.noreply.github.com>
2020-04-22 18:02:56 -0300

    Icons added

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,29 +5,42 @@ import Appbar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
 
 export default function TodoApp() {
-	const initTodos = [
-		{ id: 1, task: "Walk dog", completed: false },
-		{ id: 2, task: "Feed kids", completed: true },
-		{ id: 3, task: "Clean Darth", completed: false },
-	];
-	const [todos, setTodos] = useState(initTodos);
-	return (
-		<Paper
-			style={{
-				padding: 0,
-				margin: 0,
-				height: "100vh",
-				backgroundColor: "#fafafa",
-			}}
-			elevation={0}>
-			<Appbar color="primary" position="static" style={{ heigth: "64px" }}>
-				<Toolbar>
-					<Typography color="inherit">TODOS WITH HOOKS</Typography>
-				</Toolbar>
-			</Appbar>
-			<TodoList todos={initTodos} />
-		</Paper>
-	);
+  const initTodos = [
+    { id: 1, task: "Walk dog", completed: false },
+    { id: 2, task: "Feed kids", completed: true },
+    { id: 3, task: "Clean Darth", completed: false },
+  ];
+  const [todos, setTodos] = useState(initTodos);
+  const addTodo = (newTodoText) => {
+    setTodos([
+      ...todos,
+      { id: todos.length + 1, task: newTodoText, completed: false },
+    ]);
+  };
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+      elevation={0}
+    >
+      <Appbar color="primary" position="static" style={{ heigth: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </Appbar>
+      <Grid container justify="center" style={{ marginTop: "1rem" }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <TodoForm addTodo={addTodo} />
+          <TodoList todos={todos} />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,22 +2,18 @@ import React from "react";
 import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-
+import Todo from "./Todo";
 export default function TodoList(props) {
-	return (
-		<Paper>
-			<List>
-				{props.todos.map((todo) => (
-					<>
-						<ListItem>
-							<ListItemText>{todo.task}</ListItemText>
-						</ListItem>
-						<Divider />
-					</>
-				))}
-			</List>
-		</Paper>
-	);
+  return (
+    <Paper>
+      <List>
+        {props.todos.map((todo) => (
+          <>
+            <Todo task={todo.task} key={todo.id} completed={todo.completed} />
+            <Divider />
+          </>
+        ))}
+      </List>
+    </Paper>
+  );
 }
